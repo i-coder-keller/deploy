cb780f10020dfb7e3322bd8db7bb95363dc8ee0e
yongzhi.liu <yongzhi1.liu@gmail.com>
2022-08-03 23:43:00 +0800

    deploy

--- log4js.js
+++ log4js.js
@@ -3,7 +3,7 @@ const { getFileName } = require('./utils')
 log4js.configure({
     appenders: {
         everything: {
-            type: 'DateFile',
+            type: 'dateFile',
             filename: 'logs/log',
             pattern: '-yyyy-MM-dd.log',
             alwaysIncludePattern: true,
